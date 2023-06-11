c9dd0508c46f7ba2beb1408b4be57484b6b41259
d4kash <medakshjha@gmail.com>
2023-06-11 09:26:34 +0530

    init pushed

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./leave_management.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+app.use(bodyparser.json());
+const port = process.env.PORT || 3000;
+
+const notification_options = {
+  priority: "high",
+  timeToLive: 60 * 60 * 24,
+};
+
+app.get('/',(req,res)=>{
+  res.send("deployed sucessfully");
+})
+
+app.post("/firebase/notification", async(req, res) => {
+  
+  const registrationToken = req.body.registrationToken;
+
+  const message = {
+    notification: {
+      title: req.body.title,
+      body: req.body.body,
+      image: req.body.imageUrl,
+    }
+  };
+
+  const options = notification_options;
+
+  admin
+    .messaging()
+    .sendToDevice(registrationToken, message, options)
+    .then((response) => {
+      if (response.results[0].messageId == null) {
+        console.log(response.results[0].error);
+        res.status(400).send(response.results[0].error);
+      } else {
+        console.log(message);
+        res.status(200).send('Notification sent successfully with messageId: ' + response.results[0].messageId);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log("listening to PORT = " + port);
+});
